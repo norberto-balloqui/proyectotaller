21ee061a7af893d4012fc85d83234e1e5ad91d18
norberto-balloqui <norberto.balloqui1401@alumnos.ubiobio.cl>
2023-08-03 20:25:10 -0400

    otro front

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,18 +1,30 @@
-import { Container, Heading} from '@chakra-ui/react'
-import React from 'react'
+import { Container, Heading } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-const index = () => {
+const Index = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api');
+        console.log('Conectado al backend:', response.data);
+      } catch (error) {
+        console.error('Error al conectar al backend:', error.message);
+      }
+    };
 
+    fetchData();
+  }, []);
 
   return (
     <>
-    <Container maxW={"container.sm"} centerContent backgroundColor="blackAlpha.400" mt="10" borderRadius="10" >
-      <Heading as="h1"  textAlign="center" mt={10} mb={30}>Bienvenidos a la Sala Cuna</Heading>
-
-
-    </Container>
+      <Container maxW={'container.sm'} centerContent backgroundColor="blackAlpha.400" mt="10" borderRadius="10">
+        <Heading as="h1" textAlign="center" mt={10} mb={30}>
+          Tienda de Buzos
+        </Heading>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default index
+export default Index;
