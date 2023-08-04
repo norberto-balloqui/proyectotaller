79c83834c576f30665115fe1d1fbecd46397bda1
norberto-balloqui <norberto.balloqui1401@alumnos.ubiobio.cl>
2023-08-03 21:16:43 -0400

    asdad

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,24 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const rutacliente = require('./routes/ClienteRoutes')
-const rutainstitucion = require('./routes/InstitucionRoutes')
-const rutatalla = require('./routes/TallaRoutes')
-const rutaproducto = require('./routes/ProductoRoutes')
-const rutapedido = require('./routes/PedidoRoutes')
-const rutacarrito = require('./routes/CarritoRoutes')
-const rutacarritopro = require('./routes/CarritoProRoutes')
-const rutaestado = require('./routes/EstadoRoutes')
-const rutanotificacion = require('./routes/NotificacionRoutes')
+const rutacliente = require('./routes/ClienteRoutes');
+const rutainstitucion = require('./routes/InstitucionRoutes');
+const rutatalla = require('./routes/TallaRoutes');
+const rutaproducto = require('./routes/ProductoRoutes');
+const rutapedido = require('./routes/PedidoRoutes');
+const rutacarrito = require('./routes/CarritoRoutes');
+const rutacarritopro = require('./routes/CarritoProRoutes');
+const rutaestado = require('./routes/EstadoRoutes');
+const rutanotificacion = require('./routes/NotificacionRoutes');
 
+// Configuración de CORS
+app.use(cors({
+  credentials: false,
+  servidor: process.env.SERVIDOR, // Asegúrate de tener la variable de entorno SERVIDOR configurada adecuadamente
+  optionsSuccessStatus: 200,
+}));
 
-
-app.use(cors());
 app.use(express.json());
-app.options('*', cors());
 app.use('/api', rutacliente);
 app.use('/api', rutainstitucion);
 app.use('/api', rutatalla);
@@ -29,20 +32,16 @@ app.use('/api', rutacarritopro);
 app.use('/api', rutaestado);
 app.use('/api', rutanotificacion);
 
-
-
-app.listen(process.env.PORT, () => console.log('Server Stared'));
+app.listen(process.env.PORT, () => console.log('Server Started'));
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(process.env.DB,(err) =>
-    {
-        if (err){
-            return console.log('Error al conectar con la base de datos ->', err);
-        }
-        return console.log('Conectado a la base de datos')
-    }
-);
+mongoose.connect(process.env.DB, (err) => {
+  if (err) {
+    return console.log('Error al conectar con la base de datos ->', err);
+  }
+  return console.log('Conectado a la base de datos');
+});
